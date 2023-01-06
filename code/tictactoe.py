239d0b5579581a2578b2f4f8302ba1c96127336c
Paul Trincklin <ptrincklin@yahoo.fr>
2023-01-06 14:56:13 +0100

    push (bugué)

--- code/tictactoe.py
+++ code/tictactoe.py
@@ -4,7 +4,6 @@ import numpy as np
 fenetre=tk.Tk()
 
 size=600
-size=600
 
 Canvas=tk.Canvas(fenetre,width=size,height=size,bg='white')
 
@@ -26,7 +25,19 @@ def draw(table):
             if table[i][j]==-1:
                 Canvas.create_oval(i*l+6,j*l+6,(i+1)*l-6,(j+1)*l-6,fill="white")
 
-                                   
+def detect(X):
+    for i in range(3):
+        if X[i][0]==X[i][1] and X[i][1]==X[i][2] and X[i][0]!=0:
+            return X[i][0]
+    for j in range(3):
+        if X[0][j]==X[1][j] and X[1][j]==X[2][j] and X[0][j]!=0:
+            return X[0][j]
+    if X[0][0]==X[1][1] and X[1][1]==X[2][2] and X[0][0]!=0:
+        return X[0][0]
+    if X[0][2]==X[1][1] and X[1][1]==X[2][0] and X[0][2]!=0:
+        return X[0][2]
+    return 0
+
 def clic(event):
     global player
     global table
@@ -36,7 +47,20 @@ def clic(event):
         table[x][y]=player
         player=-player
         draw(table)
-                                   
+        winner = detect(table)
+        if winner !=0:
+            table = np.zeros((n,n))
+            Canvas.delete("all")
+            Canvas.create_text(300, 300, text=f"The winner is player {'X' if winner==1 else 'O'} !", fill="black")
+            button = tk.Button(Canvas,text="reset",command = reset())
+            button.pack()
+
+def reset():
+    print(1)
+    Canvas.delete("all")
+    table = np.zeros((n,n))
+    draw(table)
+
 draw(table)
 Canvas.bind("<Button-1>",clic)
 Canvas.pack()
